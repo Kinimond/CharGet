9ce1a8caa880eef9e659fb5d525b09b2950087d1
Patrick <patrickhass84@gmail.com>
2024-02-13 11:10:27 +0100

    Add project files.

---
+++ CharGet/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CharGet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string[] temp;
+            string charInput;
+
+            Console.WriteLine("Skriv et ord!");
+
+            input = Console.ReadLine();
+            Console.WriteLine("Dit valgte ord er: " + input);
+
+            Console.WriteLine("Vælg hvilket nummer bogstav du vil udskrive");
+            charInput = Console.ReadLine();
+
+            temp = charInput.Split(',');
+
+            for (int i = 0; i < input.Length; i++) {
+            
+            Console.WriteLine(input[int.Parse(temp[i]) -1]);
+
+        }
+            Console.ReadLine();
+        }
+    }
+}
